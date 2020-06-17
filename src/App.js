a55ea9df7124b7119cdacf17b13e841862dde721
Karina Stevens <keva86@hotmail.com>
2020-06-16 19:41:23 -0600

    added contacts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Header } from 'semantic-ui-react';
+import Contacts from './components/contacts/Contacts';
 
 class App extends Component {
-  
-  
-  
-  render () {
-    return (
-      <div> 
+  state = { contacts: [
+    { id: 1, firstName: 'John', phone: '123-123-1233'},
+    { id: 2, firstName: 'Sally', phone: '123-333-1233'},
+    { id: 3, firstName: 'Alex', phone: '123-125-1233'},
+  ]}
+  render() {
+    const { contacts } = this.state
+    return(
+      <div>
         <Header size="huge" color='blue' textAlign='center'>
           Contact list
         </Header>
+        <Contacts contacts={contacts} />
       </div>
     )
   }
 }
-
 export default App;
